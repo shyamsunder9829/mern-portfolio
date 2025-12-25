70b68fc087ceb282cb22bea40c4db97c71ee590c
shyamsunder98929 <shyamsuthar9829>
2025-12-25 16:17:20 +0530

    Fix api route path 01

--- server/server.js
+++ server/server.js
@@ -1,29 +1,25 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import contactRoute from "./routes/contactRoute.js";
+import connectDB from "./config/db.js";
+import contactRoutes from "./routes/contactRoute.js";
 
 dotenv.config();
 
 const app = express();
+
+/* 🔥 MIDDLEWARE FIRST */
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors(
-  { origin: "*",
-    methods: ["GET", "POST"],
-   }
-));
-app.use(express.json());
+/* 🔥 CONNECT DB BEFORE ROUTES */
+connectDB();
 
-app.use("/api", contactRoute);
+/* 🔥 ROUTES */
+app.use("/api", contactRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error(err));
-
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
-  console.log("MONGO URI 👉", process.env.MONGO_URI);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
